aa02fee75bf9a96e19cda0309e77c890ff7ebfab
Egg-03 <111327101+eggy03@users.noreply.github.com>
2026-02-19 10:48:56 +0530

    refactor(repository): add `@ApplicationScoped` to repositories
    - Add `@ApplicationScoped` annotation to `MessageLogRegistrationRepository`.
    - Add `@ApplicationScoped` annotation to `MessageLogContentRepository`.
    - Enable discovery by CDI

--- src/main/java/io/github/eggy03/repository/MessageLogContentRepository.java
+++ src/main/java/io/github/eggy03/repository/MessageLogContentRepository.java
@@ -2,6 +2,8 @@ package io.github.eggy03.repository;
 
 import io.github.eggy03.entity.MessageLogContent;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import jakarta.enterprise.context.ApplicationScoped;
 
+@ApplicationScoped
 public class MessageLogContentRepository implements PanacheRepositoryBase<MessageLogContent, Long> {
 }
--- src/main/java/io/github/eggy03/repository/MessageLogRegistrationRepository.java
+++ src/main/java/io/github/eggy03/repository/MessageLogRegistrationRepository.java
@@ -2,6 +2,8 @@ package io.github.eggy03.repository;
 
 import io.github.eggy03.entity.MessageLogRegistration;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import jakarta.enterprise.context.ApplicationScoped;
 
+@ApplicationScoped
 public class MessageLogRegistrationRepository implements PanacheRepositoryBase<MessageLogRegistration, Long> {
 }
