3cc0c30827977af33a33c4c2ad2aa5ab362af61a
Egg-03 <111327101+eggy03@users.noreply.github.com>
2026-02-20 08:25:35 +0530

    feat(api): add http request and response logging filter
    Signed-off-by: Egg-03 <111327101+eggy03@users.noreply.github.com>

---
+++ src/main/java/io/github/eggy03/papertrail/api/configuration/HttpLoggingFilter.java
@@ -0,0 +1,49 @@
+package io.github.eggy03.papertrail.api.configuration;
+
+import io.github.eggy03.papertrail.api.util.AnsiColor;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+import lombok.extern.slf4j.Slf4j;
+
+@Provider
+@Slf4j
+public class HttpLoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    private static final String START_TIME = "start-time";
+
+    private String colorMethod(String method) {
+        return switch (method) {
+            case "GET" -> AnsiColor.BLUE;
+            case "POST" -> AnsiColor.GREEN;
+            case "PUT" -> AnsiColor.YELLOW;
+            case "DELETE" -> AnsiColor.RED;
+            default -> AnsiColor.RESET;
+        };
+    }
+
+    @Override
+    public void filter(ContainerRequestContext containerRequestContext) {
+        containerRequestContext.setProperty(START_TIME, System.currentTimeMillis());
+    }
+
+    @Override
+    public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) {
+
+        long elapsedTime = System.currentTimeMillis() - (long) containerRequestContext.getProperty(START_TIME);
+
+        String requestMethod = containerRequestContext.getMethod();
+        String requestURI = containerRequestContext.getUriInfo().getRequestUri().getPath();
+        int responseStatus = containerResponseContext.getStatus();
+
+        log.info("{}{}{} {} -> {} ({} ms)",
+                colorMethod(requestMethod),
+                requestMethod,
+                AnsiColor.RESET,
+                requestURI,
+                responseStatus,
+                elapsedTime);
+    }
+}
