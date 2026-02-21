23ae36ad59a32dd6797c71ab60ab637d9f3cc038
Egg-03 <111327101+eggy03@users.noreply.github.com>
2026-02-21 18:27:41 +0530

    refactor(application): rename persistence class to main application
    - rename file `Persistence.java` to `MainApplication.java`
    - rename class `Persistence` to `MainApplication`
    - improve clarity of the main application entry point
    
    Signed-off-by: Egg-03 <111327101+eggy03@users.noreply.github.com>

--- src/main/java/io/github/eggy03/papertrail/api/MainApplication.java
+++ src/main/java/io/github/eggy03/papertrail/api/MainApplication.java
@@ -4,7 +4,7 @@ import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.annotations.QuarkusMain;
 
 @QuarkusMain
-public class Persistence {
+public class MainApplication {
 
     public static void main(String... args) {
         Quarkus.run(args);
