7c75de0b2d0469ef2c0e155c9171f2bab9f2ddac
Egg-03 <111327101+eggy03@users.noreply.github.com>
2026-02-28 22:48:42 +0530

    refactor(entity): replace lombok required args constructor
    - Replace `@RequiredArgsConstructor` with `@NoArgsConstructor` and `@AllArgsConstructor` in entity classes.
    - Provides a no-argument constructor for JPA compatibility and an all-argument constructor.
    
    Signed-off-by: Egg-03 <111327101+eggy03@users.noreply.github.com>

--- src/main/java/io/github/eggy03/papertrail/api/entity/AuditLogRegistration.java
+++ src/main/java/io/github/eggy03/papertrail/api/entity/AuditLogRegistration.java
@@ -4,8 +4,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -13,7 +14,8 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "audit_log_table")
 public class AuditLogRegistration {
 
