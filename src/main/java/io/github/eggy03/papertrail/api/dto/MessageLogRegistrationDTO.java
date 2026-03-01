2bbff2736e670c08bc4679ce79cbe03566a85b70
Egg-03 <111327101+eggy03@users.noreply.github.com>
2026-03-01 10:46:00 +0530

    refactor(dto): add lombok constructor annotations
    - Add `@NoArgsConstructor` and `@AllArgsConstructor` to `MessageLogContentDTO`.
    - Add `@NoArgsConstructor` and `@AllArgsConstructor` to `MessageLogRegistrationDTO`.
    - Add `@NoArgsConstructor` and `@AllArgsConstructor` to `AuditLogRegistrationDTO`.
    - Provide explicit constructors for DTOs to improve object creation flexibility.
    
    Signed-off-by: Egg-03 <111327101+eggy03@users.noreply.github.com>

--- src/main/java/io/github/eggy03/papertrail/api/dto/MessageLogRegistrationDTO.java
+++ src/main/java/io/github/eggy03/papertrail/api/dto/MessageLogRegistrationDTO.java
@@ -2,9 +2,13 @@ package io.github.eggy03.papertrail.api.dto;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MessageLogRegistrationDTO {
 
     @NotNull(message = "GuildID cannot be null")
